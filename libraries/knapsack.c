d612ec98792cf5bc83acb19100df558da7222334
Leonardo de Assis <leosil@alunos.utfpr.edu.br>
2017-03-14 15:17:40 -0300

    Change Knapsack to opaque type. Add getters and setters

--- libraries/knapsack.c
+++ libraries/knapsack.c
@@ -2,22 +2,35 @@
 #include "beam_search.h"
 #include "genetic_algorithm.h"
 
+#define TRUE 1
+#define FALSE 0
+#define NIL -1
+
+typedef struct knapsack{
+  int capacity;
+  int size;
+  int utilized_capacity;
+  int *is_included;
+  int *value;
+  int *weight;
+} Knapsack;
+
 Knapsack* create_knapsack(int size){
   Knapsack *k = malloc(sizeof(Knapsack));
+  k->capacity = NIL;
   k->size = size;
-  k->capacity = 0;
-  k->weight = malloc(sizeof(int)*size);
-  k->value = malloc(sizeof(int)*size);
+  k->utilized_capacity = 0;
   k->is_included = malloc(sizeof(int)*size);
+  k->value = malloc(sizeof(int)*size);
+  k->weight = malloc(sizeof(int)*size);
   empty_knapsack(k);
   return k;
 }
 
 void empty_knapsack(Knapsack *k){
   int i;
-
   for(i = 0; i < k->size; ++i){
-    k->is_included[i] = 0;
+    k->is_included[i] = FALSE;
   }
 }
 
@@ -25,16 +38,70 @@ void set_capacity(Knapsack *k, int capacity){
   k->capacity = capacity;
 }
 
-void set_weight(Knapsack *k, int *weight){
-  memcpy(k->weight, weight, sizeof(int)*k->size);
-}
-
 void set_value(Knapsack *k, int *value){
   memcpy(k->value, value, sizeof(int)*k->size);
 }
 
+void set_weight(Knapsack *k, int *weight){
+  memcpy(k->weight, weight, sizeof(int)*k->size);
+}
+
+int get_capacity(Knapsack *k){
+  return k->capacity;
+}
+
+int get_size(Knapsack *k){
+  return k->size;
+}
+
+int get_total_value(Knapsack *k){
+  int i, total = 0;
+  for(i = 0; i < k->size; ++i){
+    if(k->is_included[i]){
+      total += k->value[i];
+    }
+  }
+  return total;
+}
+
+int get_utilized_capacity(Knapsack *k){
+  return k->utilized_capacity;
+}
+
+int get_value(Knapsack *k, int i){
+  return k->value[i];
+}
+
+int get_weight(Knapsack *k, int i){
+  return k->weight[i];
+}
+
+int is_included(Knapsack *k, int i){
+  return k->is_included[i];
+}
+
+void add_item(Knapsack *k, int i){
+  if(!k->is_included[i]){
+    k->utilized_capacity += k->weight[i];
+    k->is_included[i] = TRUE;
+  }
+  else{
+    printf(">>> Erro: tentando adicionar item ja presente na mochila\n");
+  }
+}
+
+void remove_item(Knapsack *k, int i){
+  if(k->is_included[i]){
+    k->utilized_capacity -= k->weight[i];
+    k->is_included[i] = FALSE;
+  }
+  else{
+    printf(">>> Erro: tentando remover item nao presente na mochila\n");
+  }
+}
+
 void print_knapsack(Knapsack *k){
-  int i, total_value = 0;
+  int i, items = 0, total_value = 0;
 
   printf("\nITENS NA MOCHILA\n");
   printf("Itens [peso, valor]:");
@@ -42,20 +109,27 @@ void print_knapsack(Knapsack *k){
     if(k->is_included[i]){
       printf(" [%2d, %2d]", k->weight[i], k->value[i]);
       total_value += k->value[i];
+      ++items;
     }
   }
-  printf("\nValor total: %d\n", total_value);
+  printf("\nNumero de itens: %d\n", items);
+  printf("Peso total: %d\n", k->utilized_capacity);
+  printf("Valor total: %d\n", total_value);
 }
 
 void print_all_items(Knapsack *k){
-  int i;
+  int i, total_value = 0, total_weight = 0;
 
   printf("\nTODOS OS ITENS\n");
-  printf("Numero de itens: %d\n", k->size);
   printf("Itens [peso, valor]:");
   for(i = 0; i < k->size; ++i){
     printf(" [%2d, %2d]", k->weight[i], k->value[i]);
+    total_value += k->value[i];
+    total_weight += k->weight[i];
   }
+  printf("\nNumero de itens: %d\n", k->size);
+  printf("Peso total: %d\n", total_weight);
+  printf("Valor total: %d\n", total_value);
   printf("\n");
 }
 
--- libraries/knapsack.h
+++ libraries/knapsack.h
@@ -5,19 +5,22 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct knapsack{
-  int size;
-  int capacity;
-  int *weight;
-  int *value;
-  int *is_included;
-} Knapsack;
+typedef struct knapsack Knapsack;
 
 Knapsack* create_knapsack(int size);
 void empty_knapsack(Knapsack *k);
 void set_capacity(Knapsack *k, int capacity);
-void set_weight(Knapsack *k, int *weight);
 void set_value(Knapsack *k, int *value);
+void set_weight(Knapsack *k, int *weight);
+int get_capacity(Knapsack *k);
+int get_size(Knapsack *k);
+int get_total_value(Knapsack *k);
+int get_utilized_capacity(Knapsack *k);
+int get_value(Knapsack *k, int i);
+int get_weight(Knapsack *k, int i);
+int is_included(Knapsack *k, int i);
+void add_item(Knapsack *k, int i);
+void remove_item(Knapsack *k, int i);
 void print_knapsack(Knapsack *k);
 void print_all_items(Knapsack *k);
 void find_max_value(Knapsack *k);
